e6de840215765abebeafbf953f2ab45838e021eb
Pedro Castelao <pedro.castelao@outlook.com.br>
2023-11-13 21:15:18 -0300

    Exer resolvidos

---
+++ ListaFuncionarios/ListaFuncionarios/Funcionarios.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaFuncionarios
+{
+    internal class Funcionarios
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public double Salario { get; private set; }
+
+        public Funcionarios(int id, string nome, double salario)
+        {
+            Id = id;
+            Nome = nome;
+            Salario = salario;
+        }
+
+        public void AumentaSalario(int perc)
+        {
+            Salario += Salario * perc / 100;
+
+        }
+
+        public override string? ToString()
+        {
+            return $"\n {Id}, {Nome}, {Salario.ToString("F2", CultureInfo.InvariantCulture)}";
+        }
+    }
+}
---
+++ ListaFuncionarios/ListaFuncionarios/Program.cs
@@ -0,0 +1,45 @@
+﻿using ListaFuncionarios;
+using System.Threading.Channels;
+
+Console.WriteLine($"\n Quantos Funcionarios seram registrado ?");
+int n = int.Parse(Console.ReadLine());
+
+List<Funcionarios> list = new List<Funcionarios>();
+
+for (int i = 0; i < n; i++)
+{
+    Console.WriteLine($"\n");
+    Console.WriteLine($"Funcionario #{i}:");
+
+    Console.WriteLine("Id: ");
+    int id = int.Parse(Console.ReadLine());
+    Console.WriteLine("Nome: ");
+    string nome = Console.ReadLine();
+    Console.WriteLine("Salario: ");
+    double salario = int.Parse(Console.ReadLine());
+
+
+    list.Add(new Funcionarios(id, nome, salario));
+}
+
+Console.WriteLine($"\n Qual ID deseja aumentar o Salario ?");
+int auxId = int.Parse(Console.ReadLine());
+
+Funcionarios func = list.Find(x => x.Id == auxId);
+
+if (func != null)
+{
+    Console.WriteLine($"\n Quantos porcento quer aumentar ?");
+    int porcento = int.Parse(Console.ReadLine());
+    func.AumentaSalario(porcento);
+}
+else
+{
+    Console.WriteLine($"\n Nao tem este ID");
+}
+
+
+foreach (Funcionarios obj in list)
+{
+    Console.WriteLine(obj);
+}
